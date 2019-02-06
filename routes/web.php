1b0e536e7ab25edb63da6e9fde485a8ad344eb22
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-06 08:02:32 +0800

    telescope

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,19 @@
 |
 */
 
-// Route::get('/', 'PagesController@home');
-// Route::get('/create', 'TicketsController@create');
-// Route::post('/create', 'TicketsController@store');
-// Route::get('/tickets', 'TicketsController@index');
-// Route::get('/ticket/{slug?}', 'TicketsController@show');
-// Route::get('/ticket/{slug?}/edit','TicketsController@edit');
-// Route::post('/ticket/{slug?}/edit','TicketsController@update');
-// Route::post('/ticket/{slug?}/delete','TicketsController@destroy');
-// Route::post('/comment', 'CommentsController@newComment');
+Route::get('/', 'TicketsController@index');
+Route::get('/create', 'TicketsController@create');
+Route::post('/create', 'TicketsController@store');
+Route::get('/tickets', 'TicketsController@index');
+Route::get('/ticket/{slug?}', 'TicketsController@show');
+Route::get('/ticket/{slug?}/edit','TicketsController@edit');
+Route::post('/ticket/{slug?}/edit','TicketsController@update');
+Route::post('/ticket/{slug?}/delete','TicketsController@destroy');
+Route::post('/comment', 'CommentsController@newComment');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/', function(){
-	abort(500);
-});
+// Route::get('/', function(){
+// 	abort(500);
+// });
