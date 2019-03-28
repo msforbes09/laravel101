159c850d5f756ff6e725c4355fffd3a82643f0ce
FITPHsystemoperation <fitph.systemop@gmail.com>
2019-03-28 14:57:15 +0800

    removing local

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -21,38 +21,4 @@ class CalendarController extends Controller
     {
         return date('F j, Y', $date);
     }
-
-    // protected function today()
-    // {
-    //     // return mktime(0, 0, 0, date('n'), date('j'), date('Y'));
-    //     return mktime(0, 0, 0, 3, 2, 2019);
-    // }
-
-    // protected function numberOfWeeks($day)
-    // {
-    //     $first = date('W', mktime(0, 0, 0, date('n', $day), 2 , date('Y', $day)));
-    //     $last = date('W', mktime(0, 0, 0, date('n', $day) + 1, 1 , date('Y', $day)));
-
-    //     return $last - $first + 1;
-    // }
-
-    // protected function generateWeek($day)
-    // {
-    //     return [
-    //         'sun' => $this->showDate(
-    //                 mktime(0, 0, 0, date('n', $day), (date('j', $day) - date('w', $day)), date('Y', $day))),
-    //         'mon' => $this->showDate(
-    //                 mktime(0, 0, 0, date('n', $day), (date('j', $day) - date('w', $day)) + 1, date('Y', $day))),
-    //         'tue' => $this->showDate(
-    //                 mktime(0, 0, 0, date('n', $day), (date('j', $day) - date('w', $day)) + 2, date('Y', $day))),
-    //         'wed' => $this->showDate(
-    //                 mktime(0, 0, 0, date('n', $day), (date('j', $day) - date('w', $day)) + 3, date('Y', $day))),
-    //         'thu' => $this->showDate(
-    //                 mktime(0, 0, 0, date('n', $day), (date('j', $day) - date('w', $day)) + 4, date('Y', $day))),
-    //         'fri' => $this->showDate(
-    //                 mktime(0, 0, 0, date('n', $day), (date('j', $day) - date('w', $day)) + 5, date('Y', $day))),
-    //         'sat' => $this->showDate(
-    //                 mktime(0, 0, 0, date('n', $day), (date('j', $day) - date('w', $day)) + 6, date('Y', $day))),
-    //     ];
-    // }
 }
